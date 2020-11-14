56cddbefe433b64d8ea4bddb23f02a4784e31a76
Niket Pathak <niketpathak89@gmail.com>
2020-11-14 17:20:28 +0100

    refactor to use async, await. Remove unrequired post method from fetchWrapper

--- src/fetchWrapper/fetchWrapper.ts
+++ src/fetchWrapper/fetchWrapper.ts
@@ -1,37 +1,38 @@
 // A custom FETCH API wrapper
 // inspired by https://jasonwatmore.com/post/2020/04/18/fetch-a-lightweight-fetch-wrapper-to-simplify-http-requests
 
-const get = function (url: string): Promise<any> {
+const get = async function (url: string): Promise<any> {
   const requestOptions = {
     method: 'GET',
   };
-  return fetch(url, requestOptions).then(handleResponse);
+  const response = await fetch(url, requestOptions);
+  return handleResponse(response);
 };
 
-const post = function (url: string, body: Record<string, unknown>): Promise<any> {
-  const requestOptions = {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(body),
-  };
-  return fetch(url, requestOptions).then(handleResponse);
-};
-
-// helper functions
-const handleResponse = function (response: Response) {
-  return response.text().then((text: string) => {
-    const data = text && JSON.parse(text);
-
-    if (!response.ok) {
-      const error = (data && data.message) || response.statusText;
-      return Promise.reject(error);
-    }
-
-    return data;
-  });
+// helper function
+const handleResponse = async function (response: Response) {
+  const text = await response.text();
+  const data = text && JSON.parse(text);
+  if (!response.ok) {
+    const error = (data && data.message) || response.statusText;
+    return Promise.reject(error);
+  }
+  return data;
 };
 
 export const fetchWrapper = {
   get,
-  post,
 };
+
+/*
+ * @deprecated: Post method not required at the moment
+ */
+// const post = async function (url: string, body: unknown): Promise<any> {
+//   const requestOptions = {
+//     method: 'POST',
+//     headers: { 'Content-Type': 'application/json' },
+//     body: JSON.stringify(body),
+//   };
+//   const response = await fetch(url, requestOptions);
+//   return handleResponse(response);
+// };
