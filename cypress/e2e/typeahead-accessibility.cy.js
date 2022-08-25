648f5a891756c22288348625c46a4c43814fdb3e
Niket Pathak <niketpathak89@gmail.com>
2022-08-25 15:59:59 +0200

    ✅ test: Add test to ensure typeahead el gets added at the original position in the DOM

--- cypress/e2e/typeahead-accessibility.cy.js
+++ cypress/e2e/typeahead-accessibility.cy.js
@@ -7,8 +7,12 @@ context('Typeahead Accessibility test', () => {
   });
 
   it('markup should be accessible', () => {
+    // verify position of typeahead
+    cy.get('.input-container').children().should('have.length', 3);
+    cy.get('.input-container').children().eq(1).should('have.class', 'typeahead-standalone');
+
     cy.get('.search').type('fla', { delay: 200 });
-    cy.get('.typeahead-example-1 .tt-list').as('list').should('exist');
+    cy.get('.typeahead-accessibility-ex .tt-list').as('list').should('exist');
     cy.get('@list').children().should('have.length', 3);
     cy.get('@list').children('.tt-selected').should('have.length', 0);
 
