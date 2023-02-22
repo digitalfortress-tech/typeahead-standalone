b4364fe5230f299129a395e2910b876846266d39
Niket Pathak <niketpathak89@gmail.com>
2023-02-22 18:18:32 +0100

    ♻️ chore: refactor fetchWrapper to use better typing

--- src/fetchWrapper/fetchWrapper.ts
+++ src/fetchWrapper/fetchWrapper.ts
@@ -1,9 +1,7 @@
 // A custom FETCH API wrapper
 // inspired by https://jasonwatmore.com/post/2020/04/18/fetch-a-lightweight-fetch-wrapper-to-simplify-http-requests
 
-import { Dictionary } from '../types';
-
-const get = async function (url: string, requestOptions?: Dictionary): Promise<any> {
+const get = async function (url: RequestInfo | URL, requestOptions?: RequestInit): Promise<any> {
   const response = await fetch(
     url,
     requestOptions || {
@@ -18,8 +16,7 @@ const handleResponse = async function (response: Response) {
   const text = await response.text();
   const data = text && JSON.parse(text);
   if (!response.ok) {
-    const error = (data && data.message) || response.statusText;
-    return Promise.reject(error);
+    return Promise.reject((data && data.message) || response.statusText);
   }
   return data;
 };
