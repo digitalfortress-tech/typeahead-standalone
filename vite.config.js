5649f4eadc98892180796c9d934afe146bb12d4a
Niket Pathak <niketpathak89@gmail.com>
2023-11-13 23:21:08 +0100

    🔥✨🏗️ chore: migrate to vite, vitest and pnpm

---
+++ vite.config.js
@@ -0,0 +1,35 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { resolve, dirname } from 'path';
+import { defineConfig } from 'vite';
+// import dts from 'vite-plugin-dts';
+
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  build: {
+    target: 'es2020', // default is 'modules' which is a vite special value
+    sourcemap: 'hidden',
+    lib: {
+      entry: resolve(__dirname, 'src/typeahead-standalone.ts'),
+      name: 'typeahead',
+      fileName: (format) => `typeahead-standalone.${format}.js`,
+    },
+    rollupOptions: {
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name == 'style.css') return 'basic.css';
+          return assetInfo.name;
+        },
+      },
+    },
+  },
+  test: {
+    include: ['**/*.spec.js'],
+    globals: true,
+  },
+  server: {
+    open: '/demo/index.umd.html',
+  },
+  // This emits a types.d.ts within the dist/ folder
+  // plugins: [dts({ rollupTypes: true })],
+});
